d6d4aa18930b16a11f70c2957fc75049f0a166bc
kritika <kritika.ranswal@antsdigital.in>
2025-04-08 13:19:47 +0530

    first commit

--- app/drawer/(tabs)/_layout.tsx
+++ app/drawer/(tabs)/_layout.tsx
@@ -3,7 +3,7 @@ import { Tabs } from 'expo-router';
 export default function TabsLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="home" options={{ title: 'Home' }} />
+      <Tabs.Screen name="home" options={{ title: 'kritika' }} />
       <Tabs.Screen name="profile" options={{ title: 'Profile' }} />
     </Tabs>
   );
